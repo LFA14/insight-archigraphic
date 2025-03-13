109cafb834a7b78b7dcbc49a5c83d1be87e4f8c7
LFA14 <y.za1@hotmail.com>
2025-03-13 09:34:33 +0200

    t

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  await app.listen(3001);//this is port 
   console.log('🚀 Server is running on http://localhost:3001');
 }
 bootstrap();
